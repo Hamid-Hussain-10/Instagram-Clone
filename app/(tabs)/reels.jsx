770b95f04fcca0fb5173c1f64690ce14cf0cb052
Hamid Hussain <146005938+Hamid-Hussain-10@users.noreply.github.com>
2026-08-06 19:19:07 +0500

    include stories and posts

---
+++ app/(tabs)/reels.jsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function ReelsScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text variant="headlineMedium">Reels Screen</Text>
+    </View>
+  );
+}

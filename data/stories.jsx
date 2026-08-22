d9b11ccd2dadc2a8d46c7c35f250ff0a2e917f0d
Hamid Hussain <146005938+Hamid-Hussain-10@users.noreply.github.com>
2026-08-22 12:00:55 +0500

    UPDATED

---
+++ data/stories.jsx
@@ -0,0 +1,27 @@
+export const STORIES = [
+  {
+    id: "1",
+    name: "Your Story",
+    image: "https://i.pravatar.cc/150?img=1",
+  },
+  {
+    id: "2",
+    name: "Ali",
+    image: "https://i.pravatar.cc/150?img=2",
+  },
+  {
+    id: "3",
+    name: "Ahmed",
+    image: "https://i.pravatar.cc/150?img=3",
+  },
+  {
+    id: "4",
+    name: "Sohail",
+    image: "https://i.pravatar.cc/150?img=4",
+  },
+  {
+    id: "5",
+    name: "Jafar",
+    image: "https://i.pravatar.cc/150?img=5",
+  },
+];
